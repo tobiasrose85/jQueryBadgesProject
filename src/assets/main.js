397e823d8b0ebf5d09298ad7037ac8051d437b43
Tobias Rose - Holm Rose Laue <rose@holm-laue.de>
2017-03-19 16:16:50 +0100

    Einlesen Kurse

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,38 @@
 $(function() {
 
-  // your code will go here
+	$.ajax({
+		url: "https://www.codeschool.com/users/TobiasRose.json", 
+		dataType: 'jsonp',
+		success: function(response) {
 
+      		addCourses(response.courses.completed);
+    	}
+	});
+
+	function addCourses(courses) {
+
+      	var $badges = $('#badges');
+
+    	courses.forEach(function(course) {
+      		var $course = $('<div />',{
+      				'class': "course"
+      			}).appendTo($badges);
+      		
+      		$('<h3 />', {
+      			text : course.title
+      		}).appendTo($course);
+
+      		$('<img />',{
+      			src : course.badge
+      		}).appendTo($course);
+
+      		$('<a />',{
+      			href : course.url,
+      			text : "See Course",
+      			target : '_blank',
+      			class : "btn btn-primary"
+      		}).appendTo($course);
+      	})
+
+	}
 });
